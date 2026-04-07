2abd44881cf032cf8ee12103d5d3cb24f6b73a3a
RacineDuCiel <theogo@live.fr>
2026-04-07 08:39:53 +0200

    feat: titre onglet Bibliothèque, front port 8080, API back port 8082

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import keycloak from '../keycloak';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://localhost:8082/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
